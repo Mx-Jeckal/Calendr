f13a10852fd6bd3d282ebc0a3d254bf208e5283c
Hazel Tuxhorn <mx.jeckal@gmail.com>
2020-01-17 11:21:12 -0600

    time blocks rendered. input fields storing in localStorage. Not saving on refresh

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,74 @@
-var rowDiv = $('.row')
-var saveBtn = $('<button>')
-var tBlock = $("<div>")
-var curDay = $('#currentDay')
-var time = moment()
-
-var hoursArr = [9, 10, 11, 12, 1, 2, 3, 4, 5]
-
-// current day and date (not working)
-
-curDay.text(time.format("dddd, MMMM Mo"))
-
-for (var i = 0; i < hoursArr.length; i++) {
+$(document).ready(function() {
+    var rowDiv = $('.row')
+    var saveBtn = $('<button>')
     var tBlock = $("<div>")
-    var $tSave = $('<button>')
-    var $tInput = $('<textarea>')
+    var curDay = $('#currentDay')
 
-    tBlock.attr('class', "dayBlock")
-    tBlock.text(time.hour())
-    rowDiv.append(tBlock)
-    $tInput.attr('rows', "3")
-    $tInput.attr('cols', "20")
-    $tInput.attr('class', "input")
-    $tSave.attr('class', "saveBtn")
-    $tSave.text("save")
-    tBlock.append($tInput, $tSave)
-}
+
+    var hoursArr = ["9 A.M.", "10 A.M.", "11 A.M.", "12 P.M.", '1 P.M.', "2 P.M.", "3 P.M.", "4 P.M.", "5 P.M."]
+
+    // current day and date (not working)
+
+    curDay.text(moment().format("dddd, MMMM Do"))
+
+    // loop to generate time blocks and save buttons
+    for (var i = 0; i < hoursArr.length; i++) {
+        var tBlock = $("<div>")
+        var $tSave = $('<button>')
+        var $tInput = $('<textarea>')
+        var $tHour = $('<div>')
+
+        tBlock.attr('class', "dayBlock")
+        rowDiv.append(tBlock)
+
+        $tInput.attr('rows', "3")
+        $tInput.attr('cols', "20")
+        $tInput.attr('value', '')
+        $tInput.attr('type', "submit")
+        $tInput.attr('class', "input")
+        $tInput.attr('data-input', hoursArr[i])
+
+
+
+        $tSave.attr('class', "saveBtn")
+        $tSave.text("save")
+        $tSave.attr('data-input', hoursArr[i])
+
+        $tHour.attr('class', "hour")
+        $tHour.text(hoursArr[i])
+        tBlock.append($tHour, $tInput, $tSave)
+
+
+
+    }
+    // the click function for each save button
+    $('.saveBtn').on("click", function() {
+        var rowId = this.dataset.input
+
+        if (localStorage.getItem(hoursArr[rowId]) != null) {
+            console.log(this)
+            $('.input[data-input="' + rowId + '"]').val() = localStorage.getItem(rowId);
+        }
+
+        localStorage.setItem(rowId, $('.input[data-input="' + rowId + '"]').val())
+
+
+
+        console.log(this.dataset.input)
+
+        // if ($('.input[data-input="' + rowId + '"]').val() == "") {
+        //     alert('enter some text')
+        // } else {
+
+        //     localStorage.setItem(rowId, $('.input[data-input="' + rowId + '"]').val())
+        //     console.log(localStorage.getItem(rowId))
+        //     $(rowId).text(localStorage.getItem(rowId))
+
+        // }
+    })
+
+    // if (rowId != "") {
+    //     $('.input[data-input="' + rowId + '"]').localStorage.getItem(rowId)
+
+    // }
+})
