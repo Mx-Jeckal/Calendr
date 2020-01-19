bc17e94704ef001f08f0233014b744a8771dec06
Hazel Tuxhorn <mx.jeckal@gmail.com>
2020-01-19 11:49:43 -0600

    color coded added to hour blocks

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,12 @@
 $(document).ready(function() {
     var rowDiv = $('.row')
-    var saveBtn = $('<button>')
     var tBlock = $("<div>")
     var curDay = $('#currentDay')
 
 
-    var hoursArr = ["9 A.M.", "10 A.M.", "11 A.M.", "12 P.M.", '1 P.M.', "2 P.M.", "3 P.M.", "4 P.M.", "5 P.M."]
+    var hoursArr = ["9:00 A.M.", "10:00 A.M.", "11:00 A.M.", "12:00 P.M.", '1 P.M.', "2 P.M.", "3 P.M.", "4 P.M.", "5 P.M."]
 
-    // current day and date (not working)
-
+    // current day and date
     curDay.text(moment().format("dddd, MMMM Do"))
 
     // localDis()
@@ -20,63 +18,79 @@ $(document).ready(function() {
             }
         }
     }
-    if (moment().isBefore(hoursArr[i]))
+
+
+
     // loop to generate time blocks and save buttons
-        for (var i = 0; i < hoursArr.length; i++) {
-            localDis()
-            var tBlock = $("<div>")
-            var $tSave = $('<button>')
-            var $tInput = $('<textarea>')
-            var $tHour = $('<div>')
+    for (var i = 0; i < hoursArr.length; i++) {
+        console.log(hoursArr[i])
+        var hourIdArr = [9, 10, 11, 12, 13, 14, 15, 16, 17]
 
-            tBlock.attr('class', "dayBlock")
-            rowDiv.append(tBlock)
-
-            $tInput.attr('rows', "3")
-            $tInput.attr('cols', "20")
-            $tInput.attr('value', '')
-            $tInput.attr('type', "submit")
-            $tInput.attr('class', "input")
-            $tInput.attr('data-input', hoursArr[i])
+        var currentHour = moment().hour()
+            // console.log(moment())
 
 
+        localDis()
+        var tBlock = $("<div>")
+        var $tSave = $('<button>')
+        var $tInput = $('<textarea>')
+        var $tHour = $('<div>')
 
-            $tSave.attr('class', "saveBtn")
-            $tSave.text("save")
-            $tSave.attr('data-input', hoursArr[i])
+        tBlock.attr('class', "dayBlock")
+        tBlock.attr('data-input', hoursArr[i])
+        rowDiv.append(tBlock)
 
-            $tHour.attr('class', "hour")
-            $tHour.text(hoursArr[i])
-            tBlock.append($tHour, $tInput, $tSave)
+        $tInput.attr('rows', "3")
+        $tInput.attr('cols', "20")
+        $tInput.attr('value', '')
+        $tInput.attr('type', "submit")
+        $tInput.attr('class', "input")
+        $tInput.attr('id', hourIdArr[i])
+        $tInput.attr('data-input', hoursArr[i])
 
 
 
+        $tSave.attr('class', "saveBtn")
+        $tSave.text("save")
+        $tSave.attr('data-input', hoursArr[i])
+
+        $tHour.attr('class', "hour")
+        $tHour.text(hoursArr[i])
+        tBlock.append($tHour, $tInput, $tSave)
+
+
+        // console.log(hourIdArr[i])
+
+        if (hourIdArr[i] < (currentHour)) {
+            $($('#' + hourIdArr[i] + '')).addClass("past")
+            $('.dayBlock[data-input="' + hoursArr[i] + '"]').addClass("past")
         }
-        // the click function for each save button
+        if (hourIdArr[i] > (currentHour)) {
+            $($('#' + hourIdArr[i] + '')).addClass("future")
+            $('.dayBlock[data-input="' + hoursArr[i] + '"]').addClass("future")
+                // console.log($('#' + hourIdArr[i] + ''), ('#' + currentHour))
+                // console.log('this is future')
+        }
+        if (hourIdArr[i] == (currentHour)) {
+            $($('#' + hourIdArr[i] + '')).addClass("present")
+            $('.dayBlock[data-input="' + hoursArr[i] + '"]').addClass("present")
+                // console.log('This is present')
+        }
+
+    }
+    // the click function for each save button
     $('.saveBtn').on("click", function() {
         var rowId = this.dataset.input
 
-        console.log(localStorage.getItem(rowId))
+        // console.log(localStorage.getItem(rowId))
 
         localStorage.setItem(rowId, $('.input[data-input="' + rowId + '"]').val())
 
 
-        console.log(this.dataset.input)
+        // console.log(this.dataset.input)
 
-        // if ($('.input[data-input="' + rowId + '"]').val() == "") {
-        //     alert('enter some text')
-        // } else {
 
-        //     localStorage.setItem(rowId, $('.input[data-input="' + rowId + '"]').val())
-        //     console.log(localStorage.getItem(rowId))
-        //     $(rowId).text(localStorage.getItem(rowId))
-
-        // }
     })
 
 
-    // if (rowId != "") {
-    //     $('.input[data-input="' + rowId + '"]').localStorage.getItem(rowId)
-
-    // }
 })
